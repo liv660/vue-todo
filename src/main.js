f7bba1c4ca4bbc298a53a05e1315653b34c91b79
sysim <liv660n@gmail.com>
2021-10-05 13:15:39 +0900

    [10.05] Add bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue).use(IconsPlugin)
+
 new Vue({
   el: '#app',
   render: h => h(App)
